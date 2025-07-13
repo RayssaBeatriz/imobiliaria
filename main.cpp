9e51e8c9b64905bc9bca2c08ce846e626c70aaa6
Rayssa Beatriz <rayssa.beatriz1205@gmail.com>
2025-07-12 23:01:56 -0300

    Receber informações no main

--- main.cpp
+++ main.cpp
@@ -2,13 +2,50 @@
 #include <string>
 using namespace std;
 #include "Classes/imovel.h"
+#include "Classes/corretor.h"
 
 int main() {
-    // Criação de um objeto Imovel
-    Imovel imovel("Apartamento", 1, -23.5505, -46.6333, 500000.00, "Rua Exemplo, 123");
+    int n;
+    cin>>n;
+
+    //Criar avaliadores
+    Corretor* corretores[n];
+    for(int i = 0; i <n; i++){
+        int telefoneAvaliador;;
+        bool avaliador;
+        float lat, lng;
+        string nomeAvaliador;
+        cin >> telefoneAvaliador >> avaliador >> lat >> lng >> nomeAvaliador;
+        Corretor corretor(telefoneAvaliador, avaliador, lat, lng, nomeAvaliador);
+        corretores[i] = &corretor;
+        corretores[i]->exibirInformacoes();
+    }
+
+    cin>>n;
+    //Criar clientes
     
-    // Exibição das informações do imóvel
-    imovel.exibirInformacoes();
+    for(int i = 0; i <n; i++){
+        int telefoneCliente;
+        string nomeCliente;
+        cin >> telefoneCliente >> nomeCliente;
+    }
+
+    //Criar imóveis
+    cin>>n;
+    Imovel* imoveis[n];
+    for(int i = 0; i <n; i++){
+        string tipoImovel;
+        int proprietarioId;
+        float lat, lon;
+        double preco;
+        string endereco;
+        cin >> tipoImovel >> proprietarioId >> lat >> lon >> preco >> endereco;
+        Imovel imovel(tipoImovel, proprietarioId, lat, lon, preco, endereco);
+        imoveis[i] = &imovel;
+    }
     
+
+
     return 0;
 }
+
