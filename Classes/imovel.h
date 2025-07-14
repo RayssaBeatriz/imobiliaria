a62b653b1ec6fdec64b0c8989de6a67a8b6dad3b
Matheus André <Andre.Mattheusc@gmail.com>
2025-07-14 16:07:42 -0300

    Adiciona tipo enum e id incremental

--- Classes/imovel.h
+++ Classes/imovel.h
@@ -5,21 +5,32 @@ using namespace std;
 #ifndef IMOVEL_H
 #define IMOVEL_H
 
+enum class TipoImovel {
+    CASA, APARTAMENTO, TERRENO
+};
+
 //Classe imovel
 class Imovel {
-    private:
-    static int id;
-    string tipo;
-    int proprietarioId;
-    bool avaliador;
-    float lat;
-    float lon;
-    string endereco;
-    double preco;
-
-    public:
-    Imovel(string tipo, int proprietarioId, float lat, float lon, double preco, string endereco);
+public:
+    int id;
+    TipoImovel tipo;
+    
+    // Construtor
+    Imovel(Tipo imovel tipo, int proprietarioId, double lat, double lng, double preco, const string& endereco);
     void exibirInformacoes();
+
+    string getTipoAsString() const;
+
+private:
+    static int nextId;
+    int proprietarioId;
+    double lat;
+    double lng;
+    double preco;
+    string endereco;
 };
+
+// Função auxiliar para converter uma string de entrada para o tipo enum TipoImovel
+TipoImovel stringToTipo(const string& str);
 
 #endif
--- imp_metodos/imovel.cpp
+++ imp_metodos/imovel.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 #include "../Classes/imovel.h"
  
-//Construtor
-Imovel::Imovel(string tipo, int proprietarioId, float lat, float lon, double preco, string endereco){
-    tipo = tipo;
-    proprietarioId = proprietarioId;
-    lat = lat;
-    lon = lon;
-    preco = preco;
-    endereco = endereco;
+int Imovel::nextId = 1;
 
+//Construtor
+Imovel::Imovel(TipoImovel tipo, int proprietarioId, double lat, double lng, double preco, const string& endereco){
+    this->id = nextId++; // Atribui o ID atual e depois incrementa para o próximo
+    this->tipo = tipo;
+    this->proprietarioId = proprietarioId;
+    this->lat = lat;
+    this->lng = lng;
+    this->preco = preco;
+    this->endereco = endereco;
 }
 
 void Imovel::exibirInformacoes(){
@@ -24,4 +26,18 @@ void Imovel::exibirInformacoes(){
     cout<< "Endereço: " << endereco << endl;
 }
 
+// Converte o enum para sua representação em string
+string Imovel::getTipoAsString() const {
+    switch (tipo) {
+    case CASA: return "Casa";
+    case APARTAMENTO: return "Apartamento";
+    case TERRENO: return "Terreno";
+    }
+}
 
+// Converte uma string da entrada para o enum correspondente
+TipoImovel stringToTipo(const string& str) {
+    if (str == "casa" || str == "Casa") return CASA;
+    if (str == "apartamento" || str == "Apartamento") return APARTAMENTO;
+    if (str == "terreno" || str == "Terreno") return TERRENO;
+}
